83cddf0700bd1dca3b0248064b2a88cb9de9e630
Rich K <kbr@hey.com>
2024-06-07 22:50:54 -0400

    Added new keywords

--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -1,5 +1,4 @@
 // lexer/lexer_test.go
-
 package lexer
 
 import (
@@ -19,6 +18,12 @@ func TestNextToken(t *testing.T) {
     let result = add(five, ten);
     !-/*5;
     5 < 10 > 5;
+
+    if (5 < 10) {
+        return true;
+    } else {
+        return false;
+    }
     `
     tests := []struct {
         expectedType    token.TokenType
@@ -72,6 +77,23 @@ func TestNextToken(t *testing.T) {
         {token.GT, ">"},
         {token.INT, "5"},
         {token.SEMICOLON, ";"},
+        {token.IF, "if"},
+        {token.LPAREN, "("},
+        {token.INT, "5"},
+        {token.LT, "<"},
+        {token.INT, "10"},
+        {token.RPAREN, ")"},
+        {token.LBRACE, "{"},
+        {token.RETURN, "return"},
+        {token.TRUE, "true"},
+        {token.SEMICOLON, ";"},
+        {token.RBRACE, "}"},
+        {token.ELSE, "else"},
+        {token.LBRACE, "{"},
+        {token.RETURN, "return"},
+        {token.FALSE, "false"},
+        {token.SEMICOLON, ";"},
+        {token.RBRACE, "}"},
         {token.EOF, ""},
     }
 
