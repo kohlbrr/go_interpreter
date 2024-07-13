e644127e7afdafbfbad070b9650aab97d8902461
kohlbrr <kbr@hey.com>
2024-07-13 10:54:07 -0400

    Updating parser to handle errors

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -3,9 +3,10 @@
 package parser
 
 import (
-    "monkey/ast"
-    "monkey/lexer"
-    "monkey/token"
+	"fmt"
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
 )
 
 type Parser struct {
@@ -13,16 +14,31 @@ type Parser struct {
 
     curToken    token.Token
     peekToken   token.Token
+
+    errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-    p := &Parser{l: l}
+    p := &Parser{
+        l:      l,
+        errors: []string{},
+    }
 
     // Read two tokens, so curToken and peekToken are both set
     p.nextToken()
     p.nextToken()
 
     return p
+}
+
+func (p *Parser) Errors() []string {
+    return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+    msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+        t, p.peekToken.Type)
+    p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -34,7 +50,7 @@ func (p *Parser) ParseProgram() *ast.Program {
     program := &ast.Program{}
     program.Statements = []ast.Statement{}
 
-    for p.curToken.Type != token.EOF {
+    for !p.curTokenIs(token.EOF) { //p.curToken.Type != token.EOF {
         stmt := p.parseStatement()
         if stmt != nil {
             program.Statements = append(program.Statements, stmt)
